c87be78be10f356d5e485fc950ddc764a67426b9
Martin Wåger <martinw@gmail.com>
2014-02-19 10:25:13 +0100

    Initial project

---
+++ TDDexamplesTests/IntegerStorageTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDDexamplesTests
+{
+    [TestClass]
+    public class IntegerStorageTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
