b535a78d8983c1c1fb3616221a51fb60751d3b50
Martin Wåger <martinw@gmail.com>
2014-02-19 11:55:13 +0100

    B0001

---
+++ TDDexamplesTests/NumberDrawTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDexamples;
+
+namespace TDDexamplesTests
+{
+    [TestClass]
+    public class NumberDrawTest
+    {
+        [TestMethod]
+        public void DrawAndStore()
+        {
+            // Arrange
+            var classUnderTest = new NumberDraw();
+
+            // Act
+            classUnderTest.DrawAndStore();
+
+        }
+    }
+}
