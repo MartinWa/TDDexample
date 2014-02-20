616daa5724fe9b7336ada65c4b23f1028dfa2d18
Martin Wåger <martin.wager@dynabyte.se>
2014-02-20 13:40:00 +0100

    Övning 1 done

--- TDDexamples/NumberDraw.cs
+++ TDDexamples/NumberDraw.cs
@@ -1,10 +1,50 @@
-﻿namespace TDDexamples
+﻿using System;
+using NumberDatabaseExternalCode;
+
+namespace TDDexamples
 {
     public class NumberDraw
     {
+        private readonly IntegerStorage _integerStorage;
+        private readonly Random _rand;
+        private readonly NumberDatabase _database;
+
+        public NumberDraw()
+        {
+            _integerStorage = new IntegerStorage();
+            _rand = new Random();
+            _database = new NumberDatabase();
+        }
+
         public void DrawAndStoreNumber()
         {
-            
+            _integerStorage.Add(_rand.Next(1,20));
+        }
+
+        public string GetDrawnNumbers()
+        {
+            return _integerStorage.ToCommaSeparatedString();
+        }
+
+        public int CountDrawnNumbers()
+        {
+            return _integerStorage.Count();
+        }
+
+        public int GetNumberAtIndex(int index)
+        {
+            return _integerStorage.AtIndex(index);
+        }
+
+        public bool IsDrawn(int value)
+        {
+            return _integerStorage.IsStored(value);
+
+        }
+
+        public void WriteToDatabase()
+        {
+            _database.Store(_integerStorage.ToCommaSeparatedString());
         }
     }
 }
--- TDDexamplesTests/NumberDrawTests.cs
+++ TDDexamplesTests/NumberDrawTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TDDexamples;
 
@@ -19,6 +20,93 @@ namespace TDDexamplesTests
             // Assert
             //?
 
+        }
+
+        [TestMethod]
+        public void B0002_CommaSeparatedStringOfDrawnNumbers()
+        {
+            // Arrange
+            var classUnderTest = new NumberDraw();
+            classUnderTest.DrawAndStoreNumber();
+            classUnderTest.DrawAndStoreNumber();
+            classUnderTest.DrawAndStoreNumber();
+
+            // Act
+            var stringOfDrawnNumbers = classUnderTest.GetDrawnNumbers();
+
+            // Assert
+            Assert.IsTrue(Regex.IsMatch(stringOfDrawnNumbers, @"\d+,\d+,\d+", RegexOptions.IgnoreCase));
+        }
+
+        [TestMethod]
+        public void B0003_CountDrawnNumbers()
+        {
+            // Arrange
+            var classUnderTest = new NumberDraw();
+            classUnderTest.DrawAndStoreNumber();
+            classUnderTest.DrawAndStoreNumber();
+            classUnderTest.DrawAndStoreNumber();
+
+            // Act
+            var count = classUnderTest.CountDrawnNumbers();
+
+            // Assert
+            Assert.AreEqual(3, count);
+        }
+
+        [TestMethod]
+        public void B0004_GetNumberAtIndex()
+        {
+            // Arrange
+            var classUnderTest = new NumberDraw();
+            classUnderTest.DrawAndStoreNumber();
+            classUnderTest.DrawAndStoreNumber();
+            classUnderTest.DrawAndStoreNumber();
+
+            // Act
+            var number = classUnderTest.GetNumberAtIndex(3);
+
+            // Assert
+            Assert.IsTrue(number > 0);
+            Assert.IsTrue(number <= 20);
+        }
+
+        [TestMethod]
+        public void B0005_NumberIsDrawn()
+        {
+            // Arrange
+            var classUnderTest = new NumberDraw();
+            classUnderTest.DrawAndStoreNumber();
+            classUnderTest.DrawAndStoreNumber();
+            classUnderTest.DrawAndStoreNumber();
+
+            // Act
+            var isDrawn = classUnderTest.IsDrawn(3);
+            var isNotDrawn = classUnderTest.IsDrawn(14);
+
+
+            // Assert
+            Assert.IsTrue(isDrawn);
+            Assert.IsFalse(isNotDrawn);
+        }
+
+        [TestMethod]
+        public void B0006_DrawnNumbersStoredToDatabase()
+        {
+            // Arrange
+            var classUnderTest = new NumberDraw();
+            classUnderTest.DrawAndStoreNumber();
+            classUnderTest.DrawAndStoreNumber();
+            classUnderTest.DrawAndStoreNumber();
+
+            // Act
+            classUnderTest.WriteToDatabase();
+           
+
+
+            // Assert
+            //?
+           
         }
     }
 }
